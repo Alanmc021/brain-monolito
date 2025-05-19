1d9690860007606a3f4714401678691f0c8d3545
System Administrator <root@MXLWDW1VM6XH.local>
2025-05-19 16:58:42 -0300

    fix(app): apy key new

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-// Carrega as variáveis de ambiente antes de iniciar a aplicação
+// Carrega as variáveis de ambiente
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 7086);
+
+  // Habilita CORS para permitir chamadas do seu front-end
+  app.enableCors({
+    origin: 'http://localhost:8082', // ou '*' em desenvolvimento
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const port = process.env.PORT ?? 7086;
+  await app.listen(port);
+  console.log(`🚀 Server rodando em http://localhost:${port}`);
 }
 bootstrap();
